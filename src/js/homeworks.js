ac14863acc30abc8569664c8c51a2291edfd6503
Kenneth Ceyer <kennethan@nhpcw.com>
2017-01-13 18:44:50 +0900

    JSDoc are added.

--- src/js/homeworks.js
+++ src/js/homeworks.js
@@ -89,6 +89,15 @@ Function.prototype.hook = function (name, args) {
  *= DATE - 2016-01-19
  *================================================*/
 
+/**
+ * @constructor
+ * @description HOMEWORKS COMPONENT Store constructor, It is separated by each COMPOENTS.
+ * @see Refer an example document {@link https://kennethanceyer.gitbooks.io/homeworks-framework-wiki/content/DEVELOPMENT/ComponentData.html|here}.
+ * @author Kenneth <kenneth@igaworks.com>
+ * @param {ComponentMethod} context - HOMEWORKS ComponentMethod Context.
+ * @param {string} id -  HOMEWORKS Component Unique ID
+ * @returns {ComponentData}
+ */
 function ComponentData(context, id) {
     /*=================================================
      *= NOTE - HOMEWORKS Compoent storagy variables region.
@@ -98,55 +107,83 @@ function ComponentData(context, id) {
         _superVariables[id] = {};
     }
 
-    this.preference = {
+    /**
+     * @member
+     * @description ComponentData's store variable, This is the heart of ComponentData.
+     * @property {ComponentData} $self - Reference of ComponentData (self).
+     * @property {ComponentMethod} $super - Reference of ComponentMethod.
+     * @property {ComponentHelper} $helper - Reference of ComponentHelper.
+     * @property {Boolean} _init - If component already done initialize process, It will be true (This is prevent a duplication of initializing).
+     * @property {Boolean} _anim - The logical variable of Component animating.
+     * @property {Boolean} _bind - The logical variable of Component binding.
+     * @property {Object} anim - Animation configuration object.
+     * @property {String} framework - Framework full name.
+     * @property {String} prefix - Framework short name for using set class name prefix of Component.
+     * @property {String} id - An unique id of Component, It will be automatically setted in the process of ComponentMethod.
+     * @property {Array<jQuery>} element - Quick reference of jQuery objects that refered frequently.
+     * @property {Object} global - Global properties each of Components.
+     */
+    this.store = {
         $self: this,
         $super: context,
-        $helper: null,
+        $helper: new ComponentHelper(context, this.store),
+        _init: false,
         _anim: false,
         _bind: false,
         _debug: true,
-        _init: false,
         anim: {
             time: 300,
             effect: 'swing'
         },
         framework: 'homeworks',
         prefix: 'works',
-        id: '',
+        id: id,
         element: {
             $window: $(window),
             $document: $(document)
         },
-        global: {}
+        global: _superVariables[id] || {}
     };
-
-    this.preference.id = id;
-    this.preference.global = _superVariables[id];
-    this.preference.$helper = new ComponentHelper(context, this.preference);
 }
 
-function ComponentHelper(context, data) {
+/**
+ * @constructor
+ * @description HOMEWORKS Helper constructor, This is helping to treat DOM Elements or JS operators.
+ * @see Refer an example document {@link https://kennethanceyer.gitbooks.io/homeworks-framework-wiki/content/DEVELOPMENT/ComponentHelper.html|here}.
+ * @author Kenneth <kenneth@igaworks.com>
+ * @param {ComponentMethod} context - HOMEWORKS ComponentMethod Context.
+ * @returns {ComponentHelper}
+ */
+function ComponentHelper(context) {
     /*=================================================
      *= NOTE - HOMEWORKS Component shared feature.
      *= DATE - 2016-01-19
      *================================================*/
-    var _this = context;
 
+    /**
+     * @function
+     * @description Promiss is helping to make a schedule, It is similar like setTimeout.
+     * @param {String} name - Promiss name for control.
+     * @param {Function} callback - The callback when promiss is done.
+     * @param {Number} time - A millisecond time for promiss.
+     * @param {Function} invoke - The callback when invoke request be received.
+     * @returns {Number} setTimeout number.
+     */
     this.promise = function (name, callback, time, invoke) {
         var self = this;
         if (typeof name === 'function' && typeof callback === 'number') {
             time = callback;
             callback = name;
-            name = _this.id;
+            name = context.id;
         }
 
         if (typeof invoke !== 'undefined' && invoke === true) {
             this.invoke(name);
         }
 
-        _promiseVariables[_this.framework + '.' + name] = setTimeout(function () {
+        _promiseVariables[context.framework + '.' + name] = setTimeout(function () {
             try {
-                delete _promiseVariables[_this.framework + '.' + name];
+                delete _promiseVariables[context.framework + '.' + name];
             } catch (e) {
                 self.log(e.stack);
             }
@@ -158,9 +195,18 @@ function ComponentHelper(context, data) {
         return _promiseVariables;
     };
 
+    /**
+     * @function
+     * @description This function will invoke promiss request that you created before.
+     * @param {String} name - Promiss name for invoke a request.
+     * @param {Function} callback - The callback when promiss is done.
+     * @param {Number} time - A millisecond time for promiss.
+     * @param {Function} invoke - The callback when invoke request be received.
+     * @returns {Boolean}
+     */
     this.invoke = function (name) {
         if (typeof name === 'undefined') {
-            name = _this.id;
+            name = context.id;
         }
         if (typeof _promiseVariables[_this.framework + '.' + name] !== 'undefined') {
             try {
@@ -173,8 +219,15 @@ function ComponentHelper(context, data) {
         return true;
     };
 
+    /**
+     * @function
+     * @description The logger of HOMEWORKS Frameworks, Use this instead console.log.
+     * @param {String} message - String error message or Exception object.
+     * @param {String} code - Error code or number.
+     * @returns {undefined}
+     */
     this.log = function (message, code) {
-        if (_this._debug === true) {
+        if (context._debug === true) {
             var template = message;
 
             if (typeof message !== 'undefined' && typeof message.stack !== 'undefined') {
@@ -189,15 +242,27 @@ function ComponentHelper(context, data) {
         }
     };
 
-    this.parseTemplate = function (name, map) {
-        var data = _this.template[name];
+    /**
+     * @function
+     * @description This function find template that you declared on ComponentMethod's templates array, and than replace it using a key what you gave.
+     * @param {String} key - The template key.
+     * @param {Object} map - The template object.
+     * @returns {String}
+     */
+    this.parseTemplate = function (key, map) {
+        var data = context.template[key];
         if (typeof data === 'undefined') {
-            this.log("'" + name + "' 이름의 템플릿이 확인되지 않습니다.");
+            this.log("'" + key + "' 이름의 템플릿이 확인되지 않습니다.");
             return false;
         }
         return data.getFormat(map);
     };
 
+    /**
+     * @function
+     * @description You can get unique id for set each of component by using this function.
+     * @returns {String}
+     */
     this.getIdentifier = function () {
         var id = data.id.replace(/,?\s/g, '-');
         var id_arr = id.split('');
@@ -205,6 +270,15 @@ function ComponentHelper(context, data) {
         return data.framework + id;
     };
 
+    /**
+     * @function
+     * @description It will attach an event to your element, Use this function instead jQuery.bind.
+     * @param {jQuery} element - jQuery object for attach event.
+     * @param {String} type - Event type name.
+     * @param {Function} callback - The callback function which fired when event triggered.
+     * @param {Boolean} initialize - If this parameter is true, The event is triggered once automatically.
+     * @returns {undefined}
+     */
     this.bind = function (element, type, callback, initialize) {
         try {
             var forms = type.toString().split(' ');
@@ -232,40 +306,92 @@ function ComponentHelper(context, data) {
         }
     };
 
+    /**
+     * @function
+     * @description Deattach event from the jQuery object, Use this function instead jQuery.unbind.
+     * @param {jQuery} element - jQuery object for deattach event.
+     * @param {String} type - Event type name.
+     * @returns {undefined}
+     */
     this.unbind = function (element, type) {
         element.unbind(type + '.' + this.getIdentifier());
     };
 
+    /**
+     * @function
+     * @description This function will trigger an event, Use this function instead jQuery.trigger.
+     * @param {jQuery} element - jQuery object for trigger event.
+     * @param {String} type - Event type name.
+     * @param {Any} value - Extra values for sending the binder function.
+     * @returns {undefined}
+     */
     this.trigger = function (element, type, value) {
         var forms = (type.toString().split(' '))[0];
         element.trigger(value === true ? forms : (forms + '.' + this.getIdentifier()), value);
     };
 
+    /**
+     * @function
+     * @description This function will trigger an event "only logically", Use this function instead jQuery.triggerHandler.
+     * @param {jQuery} element - jQuery object for trigger event logically.
+     * @param {String} type - Event type name.
+     * @param {Any} value - Extra values for sending the binder function.
+     * @returns {undefined}
+     */
     this.triggerHandler = function (element, type, value) {
         var forms = (type.toString().split(' '))[0];
         element.triggerHandler(value === true? forms : (forms + '.' + this.getIdentifier()), value);
     };
 }
 
+/**
+ * @constructor
+ * @description HOMEWORKS COMPONENT Create manager, This is core constructor for making HOMEWORKS COMPONENT.
+ * @see Refer an example document {@link https://kennethanceyer.gitbooks.io/homeworks-framework-wiki/content/DEVELOPMENT/ComponentMethod.html|here}.
+ * @author Kenneth <kenneth@igaworks.com>
+ * @param {string} name - COMPONENT Name.
+ * @params {Object} settings - COMPONENT Settings.
+ * @returns {ComponentMethod}
+ */
 function ComponentMethod(name, settings) {
     /*=================================================
      *= NOTE - HOMEWORKS Component settings region.
      *= DATE - 2016-01-19
      *================================================*/
-    var _this = this;
-    var _componentVariables = (new ComponentData(this, name)).preference;
+    var context = this;
+
+    var _componentVariables = (new ComponentData(this, name)).store;
     var _globalVariables = _componentVariables.global;
 
     if (typeof settings.options !== 'undefined' && settings.options !== null) {
         $.extend(_superVariables[name], settings.options);
     }
 
+    /**
+     * @member {Function}
+     * @description The constructor of Component, This function will be called when Component mapping be started.
+     */
     this.init = settings.init;
+
+    /**
+     * @member {Object<Function>}
+     * @description An object wrapped functions, This object is declared Component methods.
+     */
     this.method = {
         init: this.init
     };
+
+    /**
+     * @member {Object<String>}
+     * @description An object wrapped templates, This object is declared templates which use in Component.
+     */
     this.template = {
     };
+
+    /**
+     * @member {Object<Any>}
+     * @description Options decleared by caller, You can default option in ComponentMethod.
+     */
     this.options = {
     };
 
@@ -277,6 +403,13 @@ function ComponentMethod(name, settings) {
      *= NOTE - HOMEWORKS ROUTE START
      *= DATE - 2017-01-10
      *================================================*/
+
+    /**
+     * @function
+     * @description This function is core of ComponentMethod, This is provider to collecting Component by call patterns.
+     * @param {string} id - Component unique id for give to internel of Component.
+     * @returns {jQuery}
+     */
     this.route = function (id) {
         var self = this;
         var args = [];
@@ -294,14 +427,14 @@ function ComponentMethod(name, settings) {
                         '_id': id,
                         '_init': false,
                         '_prototype': {},
-                        '_options': $.extend({}, _this.options)
+                        '_options': $.extend({}, context.options)
                     };
                     this.data = _localVariables;
-                    $.extend(_localVariables._prototype, _this.method);
+                    $.extend(_localVariables._prototype, context.method);
                 }
             }
 
-            var context = $.extend(this, {
+            var componentContext = $.extend(this, {
                 local: _localVariables,
             }, _componentVariables);
 
@@ -314,20 +447,20 @@ function ComponentMethod(name, settings) {
 
                 if (_localVariables._init === false) {
                     _localVariables._init = true;
-                    _this.init.apply(context, [$(this)].concat(Array.prototype.slice.call(args)));
+                    context.init.apply(componentContext, [$(this)].concat(Array.prototype.slice.call(args)));
                 }
             } else if (typeof args[0] === 'string') {
                 // Function(Method Name) pattern.
                 try {
                     if (_localVariables._init === false) {
                         _localVariables._init = true;
-                        if (typeof _this.method.init !== 'undefined') {
-                            _this.method.init.apply(context, [$(this)].concat(Array.prototype.slice.call(args, 1)));
+                        if (typeof context.method.init !== 'undefined') {
+                            context.method.init.apply(componentContext, [$(this)].concat(Array.prototype.slice.call(args, 1)));
                         } else {
-                            _this.init.apply(context, [$(this)].concat(Array.prototype.slice.call(args, 1)));
+                            context.init.apply(componentContext, [$(this)].concat(Array.prototype.slice.call(args, 1)));
                         }
                     }
-                    return _this.method[args[0]].apply(context, [$(this)].concat(Array.prototype.slice.call(args, 1)));
+                    return context.method[args[0]].apply(componentContext, [$(this)].concat(Array.prototype.slice.call(args, 1)));
                 } catch (e) {
                     _componentVariables.$helper.log(e.stack);
                 }
@@ -345,16 +478,16 @@ function ComponentMethod(name, settings) {
                         '_id': id,
                         '_init': false,
                         '_prototype': {},
-                        '_options': $.extend({}, _this.options)
+                        '_options': $.extend({}, context.options)
                     };
                     this.data = _localVariables;
-                    $.extend(_localVariables._prototype, _this.method);
+                    $.extend(_localVariables._prototype, context.method);
                 }
             }
-            var context = $.extend(window, {
+            var componentContext = $.extend(window, {
                 local: _localVariables,
             }, _componentVariables);
-            _this.method.init.apply(context, Array.prototype.slice.call(args));
+            context.method.init.apply(componentContext, Array.prototype.slice.call(args));
         } else {
             // By element channing method type - Elem.method()
             if (typeof self !== 'undefined') {
@@ -382,7 +515,7 @@ function ComponentMethod(name, settings) {
             !(function () {
                 var _id = id;
                 var bindFunc = function () {
-                    return _this.route.apply(this, [_id].concat(Array.prototype.slice.call(arguments)));
+                    return context.route.apply(this, [_id].concat(Array.prototype.slice.call(arguments)));
                 };
                 $.fn[_id] = bindFunc;
                 window[_id] = bindFunc;
@@ -410,24 +543,24 @@ function ComponentMethod(name, settings) {
                                         '_options': {}
                                     };
                                     element.data = _localVariables;
-                                    $.extend(_localVariables._prototype, _this.method);
+                                    $.extend(_localVariables._prototype, context.method);
                                 }
 
-                                var context = $.extend(_this, {
+                                var componentContext = $.extend(context, {
                                     local: _localVariables,
                                 }, _componentVariables);
                             }
 
                             if (_localVariables._init === false) {
                                 _localVariables._init = true;
-                                if (typeof _this.method.init !== 'undefined') {
-                                    _this.method.init.apply(context, [$(this)].concat(Array.prototype.slice.call(arg, 1)));
+                                if (typeof context.method.init !== 'undefined') {
+                                    context.method.init.apply(componentContext, [$(this)].concat(Array.prototype.slice.call(arg, 1)));
                                 } else {
-                                    _this.init.apply(context, [$(this)].concat(Array.prototype.slice.call(arg, 1)));
+                                    context.init.apply(componentContext, [$(this)].concat(Array.prototype.slice.call(arg, 1)));
                                 }
                             }
 
-                            return _this.method[method].apply(context, [this].concat(Array.prototype.slice.call(arguments)));
+                            return context.method[method].apply(componentContext, [this].concat(Array.prototype.slice.call(arguments)));
                         };
                     }());
                 }
